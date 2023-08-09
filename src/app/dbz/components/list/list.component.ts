fa852400f440a2255f763e9687510cebe1be50d8
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-08-09 16:59:11 -0400

    primer commit

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+import { v4 as uuid } from 'uuid'
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent{
+
+  @Input()
+  public characterList: Character[] = [
+    {
+      id: uuid(),
+      name: 'Trunks',
+      power: 10
+    }
+  ]
+
+  @Output()
+  public onDelete:EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id: string):void{
+    this.onDelete.emit(id);
+  }
+}
